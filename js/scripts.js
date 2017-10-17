edcab940760ff5aebc6e05762d3f6cba139a0e32
amy churchwell and shane ryan <amy@>
2017-10-17 13:05:50 -0700

    add definitions complete js functions

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,71 @@ $(document).ready(function(){
   $("#variables-title").click(function(){
     $("#variables").fadeToggle();
   });
-  
+
+  //CARD SET 2//
+
+  $("#var-naming-title").click(function(){
+    $("#var-naming").fadeToggle();
+  });
+
+  $("#functions-title").click(function(){
+    $("#functions").fadeToggle();
+  });
+
+  $("#methods-title").click(function(){
+    $("#methods").fadeToggle();
+  });
+  //CARD SET 3//
+  $("#arguments-title").click(function(){
+    $("#arguments").fadeToggle();
+  });
+
+  $("#parameters-title").click(function(){
+    $("#parameters").fadeToggle();
+  });
+
+  $("#return-title").click(function(){
+    $("#return").fadeToggle();
+  });
+//CARD SET 4//
+  $("#chaining-title").click(function(){
+    $("#chaining").fadeToggle();
+  });
+
+  $("#strings-title").click(function(){
+    $("#strings").fadeToggle();
+  });
+
+  $("#booleans-title").click(function(){
+    $("#booleans").fadeToggle();
+  });
+//CARD SET 5//
+  $("#undefined-title").click(function(){
+    $("#undefined").fadeToggle();
+  });
+
+  $("#nan-title").click(function(){
+    $("#nan").fadeToggle();
+  });
+
+  $("#escape-title").click(function(){
+    $("#escape").fadeToggle();
+  });
+//CARD SET 6//
+  $("#alert-title").click(function(){
+    $("#alert").fadeToggle();
+  });
+
+  $("#comments-title").click(function(){
+    $("#comments").fadeToggle();
+  });
+
+  $("#jquery-title").click(function(){
+    $("#jquery").fadeToggle();
+  });
+
+  $("#attribute-title").click(function(){
+    $("#attribute").fadeToggle();
+  });
+
 });
