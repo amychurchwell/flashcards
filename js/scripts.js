1265d02a1097e0353de326d5c88197a3ccb7ea1b
amy churchwell and shane ryan <shane@>
2017-10-17 09:59:03 -0700

    add functioning card structure to hide and show definitions

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#javascript-title").click(function(){
+    $("#javascript").fadeToggle();
+  });
+
+  $("#operators-title").click(function(){
+    $("#operators").fadeToggle();
+  });
+
+  $("#variables-title").click(function(){
+    $("#variables").fadeToggle();
+  });
+  
+});
